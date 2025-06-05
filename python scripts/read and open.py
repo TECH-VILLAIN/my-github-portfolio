05a808a42a2945cc9c46f9ed34c71368756cce63
Mbaegbu Chidiebere Johnpaul <mbaegbujpchidiebere@gmail.com>
2025-06-05 18:53:54 +0100

    files updated

---
+++ python scripts/read and open.py
@@ -0,0 +1,14 @@
+with open("c:\\Users\\TECH_VILLAIN\\Documents\\message.txt",'a') as file1:
+     file1.write("i am a man in an animal form\n")
+     file1.write('i am hipo\n')
+     file1.write('somto')
+     file1.write('dera')
+          
+with open("c:\\Users\\TECH_VILLAIN\\Documents\\message.txt",'r') as file1:
+         content = file1.read()
+         print(content)
+for char in content:
+     print(char)
+contents = content.split()
+count_words = len(contents)
+print(count_words)
