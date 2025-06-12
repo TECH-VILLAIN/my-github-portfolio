f2d8a21b0d4373abb436a5f822f762f9ea1110c2
Mbaegbu Chidiebere Johnpaul <mbaegbujpchidiebere@gmail.com>
2025-06-12 19:44:49 +0100

    modified

---
+++ Data Science/API-projects/visualizing 7 day weather forecast for a city.py
@@ -0,0 +1,22 @@
+import requests
+import matplotlib.pyplot as plt
+API_KEY = "8c28a04fab606d0bdb006609726337a9"
+LAT = input("enter latitude")
+LON = input("enter longitude")
+URL = "https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LON}&exclude=current,minutely,hourly,alerts&units=metric&appid=8c28a04fab606d0bdb006609726337a9"
+response = requests.get(URL)
+data = response.json()
+days= []
+temps= []
+for day in data["daily"]:
+    days.append(day["dt"])
+    temps.append(day["temp"]["day"])
+from datetime import datetime
+days = [datetime.fromtimestamp(d).strftime('%a %d') for d in days]
+plt.figure(figsize=(10, 5))
+plt.plot(days, temps, marker='o', color='skyblue')
+plt.title("7-Day Weather Forecast for Lagos 🌤️")
+plt.xlabel("Day")
+plt.ylabel("Temperature (°C)")
+plt.grid(True)
+plt.show()
--- Data Science/API-projects/weather forcast.py
+++ Data Science/API-projects/weather forcast.py
@@ -1,7 +1,7 @@
 import requests
-API_KEY = "8c28a04fab606d0bdb006609726337a9"
+API_KEY = ""
 CITY = input("enter city name:")
-URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid=8c28a04fab606d0bdb006609726337a9&units=metric"
+URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid=&units=metric"
 response = requests.get(URL)
 data = response.json()
 if response.status_code == 200:
@@ -10,3 +10,22 @@ if response.status_code == 200:
     print(f"Description: {data['weather'][0]['description']}")
 else:
     print("Failed to retrieve data:", data.get("message"))
+
+import os
+
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+CITY = "Lagos"
+URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
+
+response = requests.get(URL)
+data = response.json()
+
+if response.status_code == 200:
+    print(f"Weather in {CITY}:")
+    print(f"Temperature: {data['main']['temp']}°C")
+    print(f"Description: {data['weather'][0]['description']}")
+else:
+    print("Failed to retrieve data:", data.get("message"))
+
+
